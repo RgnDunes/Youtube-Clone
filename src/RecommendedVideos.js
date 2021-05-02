46604eee47988b03fae29a77b1b551de192e6064
RgnDunes <singh.divyansh1802@gmail.com>
2021-05-03 04:52:03 +0530

    Added Recommended Videos Section

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,61 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h1>Videos Here</h1>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+        <VideoCard
+          image="https://avatars.githubusercontent.com/u/61814592?v=4"
+          title="Divyansh Singh Github"
+          channel="RgnDunes"
+          views="1234"
+          timestamp="3 days ago"
+          channelImage="https://avatars.githubusercontent.com/u/61814592?v=4"
+        />
+      </div>
     </div>
   );
 }
